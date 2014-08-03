3590d61c1fb3ad06802ad32d4594f4ecf5898bc4
Bart Middag <Bart.Middag@UGent.be>
2014-08-03 23:34:24 +0200

    SDL Tutorial 02 - Drawing Blue
    And so it begins.

---
+++ Dreambound/main.cpp
@@ -0,0 +1,154 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <stdio.h>
+#include <string>
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 1080;
+const int SCREEN_HEIGHT = 720;
+
+//Starts up SDL and creates window
+bool init();
+
+//Loads media
+bool loadMedia();
+
+//Frees media and shuts down SDL
+void close();
+
+//Loads individual image
+SDL_Surface* loadSurface( std::string path );
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+	
+//The surface contained by the window
+SDL_Surface* gScreenSurface = NULL;
+
+//Current displayed image
+SDL_Surface* gPNGSurface = NULL;
+
+bool init() {
+	//Initialization flag
+	bool success = true;
+
+	//Initialize SDL
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
+		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+		success = false;
+	} else {
+		//Create window
+		gWindow = SDL_CreateWindow( "Blue met SDL_Image", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		if( gWindow == NULL ) {
+			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+			success = false;
+		} else {
+			//Initialize PNG loading
+			int imgFlags = IMG_INIT_PNG;
+			if( !( IMG_Init( imgFlags ) & imgFlags ) ) {
+				printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+				success = false;
+			} else {
+				//Get window surface
+				gScreenSurface = SDL_GetWindowSurface( gWindow );
+			}
+		}
+	}
+
+	return success;
+}
+
+
+bool loadMedia() {
+	//Loading success flag
+	bool success = true;
+
+	//Load stretching surface
+	gPNGSurface = loadSurface( "template_blue.png" );
+	if( gPNGSurface == NULL ) {
+		printf( "Failed to load stretching image!\n" );
+		success = false;
+	}
+
+	return success;
+}
+
+void close() {
+	//Deallocate surface
+	SDL_FreeSurface(gPNGSurface);
+	gPNGSurface = NULL;
+
+	//Destroy window
+	SDL_DestroyWindow( gWindow );
+	gWindow = NULL;
+
+	//Quit SDL subsystems
+	SDL_Quit();
+}
+
+SDL_Surface* loadSurface( std::string path ) {
+	//The final optimized image
+	SDL_Surface* optimizedSurface = NULL;
+
+	//Load image at specified path
+	SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+	if( loadedSurface == NULL ) {
+		printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
+	} else {
+		//Convert surface to screen format
+		optimizedSurface = SDL_ConvertSurface( loadedSurface, gScreenSurface->format, NULL );
+		if( optimizedSurface == NULL ) {
+			printf( "Unable to optimize image %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+		}
+
+		//Get rid of old loaded surface
+		SDL_FreeSurface( loadedSurface );
+	}
+
+	return optimizedSurface;
+}
+
+int main( int argc, char* args[] ) {
+	//Start up SDL and create window
+	if( !init() ) {
+		printf( "Failed to initialize!\n" );
+	} else {
+		//Load media
+		if( !loadMedia() ) {
+			printf( "Failed to load media!\n" );
+		} else {	
+			//Main loop flag
+			bool quit = false;
+
+			//Event handler
+			SDL_Event e;
+
+			//While application is running
+			while( !quit ) {
+				//Handle events on queue
+				while( SDL_PollEvent( &e ) != 0 ) {
+					//User requests quit
+					if( e.type == SDL_QUIT ) {
+						quit = true;
+					}
+				}
+
+				//Apply the image stretched
+				SDL_Rect stretchRect;
+				stretchRect.x = SCREEN_WIDTH/2 - 458/2;
+				stretchRect.y = SCREEN_HEIGHT/2 - 496/2;
+				stretchRect.w = 458;
+				stretchRect.h = 496;
+				SDL_BlitScaled( gPNGSurface, NULL, gScreenSurface, &stretchRect );
+			
+				//Update the surface
+				SDL_UpdateWindowSurface( gWindow );
+			}
+		}
+	}
+
+	//Free resources and close SDL
+	close();
+
+	return 0;
+}
