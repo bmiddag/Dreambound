a10b33fc2061ab533550c041b00269cc97e461bf
Bart Middag <Bart.Middag@UGent.be>
2014-08-07 17:28:46 +0200

    Font & FPS counter test

--- Dreambound/main.cpp
+++ Dreambound/main.cpp
@@ -1,10 +1,26 @@
 #include <SFML/Graphics.hpp>
+#include <sstream>
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(720, 480), "SFML works!");
+	sf::RenderWindow window(sf::VideoMode(1080, 720), "SFML works!");
 	sf::CircleShape shape(240.f);
 	shape.setFillColor(sf::Color::Green);
+
+	sf::Font testFont;
+	if (!testFont.loadFromFile("arial.ttf"))
+	{
+		return 0;
+	}
+
+	sf::Clock clock;
+	sf::Text fpsText;
+	fpsText.setColor(sf::Color::White);
+	fpsText.setFont(testFont);
+	float lastTime = 0;
+	float currentTime;
+	float fps;
+	std::ostringstream ss;
 
 	while (window.isOpen())
 	{
@@ -15,9 +31,17 @@ int main()
 				window.close();
 		}
 
+		currentTime = clock.restart().asSeconds();
+		fps = 1.f / (currentTime - lastTime);
+		lastTime = currentTime;
+		ss << "FPS: " << fps;
+		fpsText.setString(ss.str());
+
 		window.clear();
 		window.draw(shape);
+		window.draw(fpsText);
 		window.display();
+		ss.str(std::string());
 	}
 
 	return 0;
