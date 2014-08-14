bffdf83f0402795e44b295f9aa51f89572a6f4c2
Thamasky <thamasky@hotmail.com>
2014-08-15 01:32:03 +0200

    Added blank methods
    Added blank methods in Game.cpp

--- Dreambound/Game.cpp
+++ Dreambound/Game.cpp
@@ -4,3 +4,44 @@
 Game::Game(){
 
 }
+
+bool init(){
+
+}
+
+void loop(){
+
+}
+
+void beginStep(){
+
+}
+
+void step(){
+
+}
+
+void endStep(){
+
+}
+
+void cleanup(){
+
+}
+
+
+// EVENTS
+void registerStep(Object object){
+}
+
+void registerBeginStep(Object object){
+}
+
+void registerEndStep(Object object){
+}
+
+void registerChangeListener(Object object){
+}
+
+void registerRender(Object object){
+}
