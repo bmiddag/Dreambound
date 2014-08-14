bd311a83969260aab9b3f94ff6fdd2a0f956de40
Bart Middag <Bart.Middag@UGent.be>
2014-08-14 03:36:38 +0200

    Basic stuff...

--- Dreambound/main.cpp
+++ Dreambound/main.cpp
@@ -8,7 +8,7 @@ Every object has several functions that contain game logic.
 - begin step (called before all step functions)
 - step (happens every 1/60th of a second)
 - end step (called after all step functions are finished)
-- draw (called by a separate rendering thread every 1/60th of a second. The separate thread allows for frameskip if the framerate drops)
+- draw (called every 1/60th of a second. Frameskip if the framerate drops)
 Every object can have different types. Examples are:
 - actor (any playable or non-playable character). Those have playable, friendly, or enemy subtype.
 - solid (platforms you can walk on). These should be defined by just vector lines.
@@ -16,15 +16,13 @@ Every object can have different types. Examples are:
 There's a master object for every "scene" (= every separate environment) that controls lists of actors, solids, etc. and unloads most of these at a scene change. */
 // TODO: After implementing the game system, remove FPS counter from this file and add as separate game object
 
-int main()
-{
+int main() {
 	sf::RenderWindow window(sf::VideoMode(1080, 720), "SFML works!");
 	sf::CircleShape shape(240.f);
 	shape.setFillColor(sf::Color::Green);
 
 	sf::Font testFont;
-	if (!testFont.loadFromFile("arial.ttf"))
-	{
+	if (!testFont.loadFromFile("arial.ttf")) {
 		return 0;
 	}
 
@@ -36,22 +34,18 @@ int main()
 	float fps;
 	std::ostringstream ss;
 
-	// 1. Start separate rendering thread which executes the draw instruction once every 1/60th second
-
-	while (window.isOpen())
-	{
+	while (window.isOpen()) {
 		// Every 1/60th second:
-		// 2. Handle window events here
+		// Handle window events here
 		sf::Event event;
-		while (window.pollEvent(event))
-		{
+		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
 		// GAME LOGIC (speed can be configured, pass supposed fps to all objects!)
-		// 3. Execute Begin Step instructions (those with begin step events register to Begin Step listener)
-		// 4. Execute Step instructions (those with step events register to Step listener)
-		// 5. Execute End Step instructions (those with end step events register to End Step listener)
+		// Execute Begin Step instructions (those with begin step events register to Begin Step listener)
+		// Execute Step instructions (those with step events register to Step listener)
+		// Execute End Step instructions (those with end step events register to End Step listener)
 
 		currentTime = clock.restart().asSeconds();
 		fps = 1.f / (currentTime);
