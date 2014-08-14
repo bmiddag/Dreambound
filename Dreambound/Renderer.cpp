b06ff08e6147cd397d4f4880a74b50ccdef3f4d2
Thamasky <thamasky@hotmail.com>
2014-08-15 01:37:14 +0200

    Added blank methods
    Added blank methods for Renderer.cpp

--- Dreambound/Renderer.cpp
+++ Dreambound/Renderer.cpp
@@ -1,6 +1,27 @@
 #include "Renderer.hpp"
 
+// Basic Renderer constructor
+Renderer::Renderer(){
+
+}
+
 // Renderer constructor
 Renderer::Renderer(sf::RenderWindow canvas){
+
+}
+
+bool init(){
+
+}
+
+void render(){
+
+}
+
+void cleanup(){
+
+}
+
+void registerRender(Object object){
 
 }
